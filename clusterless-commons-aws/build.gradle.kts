269203f12739791d9dd5528145c572b257196928
Chris K Wensel <chris@wensel.net>
2023-10-23 20:19:38 -0700

    remove unused dependency

--- clusterless-commons-aws/build.gradle.kts
+++ clusterless-commons-aws/build.gradle.kts
@@ -14,10 +14,8 @@ dependencies {
     implementation(project(":clusterless-commons-core"))
 
     // https://mvnrepository.com/artifact/software.amazon.awscdk/aws-cdk-lib
-
     val cdkVersion = "2.100.0"
     api("software.amazon.awscdk:aws-cdk-lib:$cdkVersion")
-    api("software.amazon.awscdk:glue-alpha:$cdkVersion-alpha.0")
     // https://mvnrepository.com/artifact/software.constructs/constructs
     val constructsVersion = "10.3.0"
     api("software.constructs:constructs:$constructsVersion")
