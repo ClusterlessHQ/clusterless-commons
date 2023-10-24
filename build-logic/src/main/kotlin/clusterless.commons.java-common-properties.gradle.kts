97bd55e6e39e415ae8fbe15a636b0aec879aa861
Chris K Wensel <chris@wensel.net>
2023-10-24 13:18:00 -0700

    version 0.6

--- build-logic/src/main/kotlin/clusterless.commons.java-common-properties.gradle.kts
+++ build-logic/src/main/kotlin/clusterless.commons.java-common-properties.gradle.kts
@@ -7,7 +7,7 @@
 */
 
 extra["group"] = "io.clusterless"
-extra["version"] = "0.5"
+extra["version"] = "0.6"
 
 extra["repoUserName"] = System.getProperty("publish.repo.userName")
 extra["repoPassword"] = System.getProperty("publish.repo.password")
